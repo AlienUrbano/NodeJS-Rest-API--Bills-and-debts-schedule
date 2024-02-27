e6c08ebe193168df2fb06b6e3ce4e4f18c628477
AlienUrbano <paulogabrielneves@hotmail.com>
2024-02-27 11:52:40 -0300

    feat: add endpoints to create, list, detail, update and delete bills or debts

--- index.js
+++ index.js
@@ -4,8 +4,42 @@ const app = express()
 
 const port = 3000 || process.env.PORT
 
-app.get('/', (req, res) => {
-    return res.json({msg:'running...'})
+app.use(express.json())
+
+
+// Schedules a bill or debt
+app.post('/bill', (req, res) => {
+    const data = req.body
+    return res.json(data)
+})
+
+// Lists all bills and debts 
+app.get('/bills', (req, res) => {
+    return res.json({msg:'List bills...'})
+})
+
+// Details a bill or debt 
+app.get('/bill/:id', (req, res) => {
+
+    const id = req.params.id
+
+    return res.json({msg:`detail of bill #${id}` })
+})
+
+// update a bill or debt 
+app.put('/bill/:id', (req, res) => {
+
+    const id = req.params.id
+
+    return res.json({msg: `update bill #${id}`})
+})
+
+// delete a bill or debt 
+app.delete('/bill/:id', (req, res) => {
+
+    const id = req.params.id
+
+    return res.json({msg: `delete bill #${id}`})
 })
 
 app.listen(port, () => {
