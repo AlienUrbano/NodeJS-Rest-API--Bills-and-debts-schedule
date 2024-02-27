64f54eed5a13383f9ec049cb6110db631c77dfdc
AlienUrbano <paulogabrielneves@hotmail.com>
2024-02-27 11:18:31 -0300

    build setup express, nodemon and  npm start

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("running...");
+const express = require ('express')
+
+const app = express()
+
+const port = 3000 || process.env.PORT
+
+app.get('/', (req, res) => {
+    return res.json({msg:'running...'})
+})
+
+app.listen(port, () => {
+    console.log(`running @localhost:${port}`)
+})
